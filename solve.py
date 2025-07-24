dd8c2a88cc2aad30ede09ed29b4fc2b95eec5714
Prajwal Udupa <uprajwal05@gmail.com>
2025-07-24 09:03:42 +0530

    Initial commit

---
+++ solve.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "http://verbal-sleep.picoctf.net:50765/impossibleLogin.php"
+pdf1 = "https://shattered.io/static/shattered-1.pdf"
+pdf2 = "https://shattered.io/static/shattered-2.pdf"
+
+pdf1bytes = requests.get(pdf1).content
+pdf2bytes = requests.get(pdf2).content
+
+data = {
+"username" : pdf1bytes,
+"pwd" : pdf2bytes
+}
+
+res =requests.post(url, data=data)
+print(res.text) 
